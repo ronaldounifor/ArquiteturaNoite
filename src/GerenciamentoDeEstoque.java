dd7b71325eef8c69a96736f2750d85476c1587ab
ronaldounifor <ronaldo@unifor.br>
2022-08-09 20:19:02 -0300

    Segundo commit

---
+++ src/GerenciamentoDeEstoque.java
@@ -0,0 +1,18 @@
+public class GerenciamentoDeEstoque {
+    //Criar classe Produto
+    public void adicionaProduto() {}
+    public void pesquisaProduto() {}
+    public void exclui() {}
+    public void atualizarDescricaoProduto() {}
+
+    //Classe Estoque
+//    public void adicionaProduto() {}
+
+    public void quantidadeTotalDeProdutos() {}
+    public void quantidadeDeUmProdutoEspecifico() {}
+    public void tirarDoEstoque() {}
+    public void valorTotalEmEstoque() {}
+    public void lote() {}
+    public void endereco() {}
+    public void produtosEmFalta() {}
+}
